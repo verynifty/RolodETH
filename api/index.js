6f5ce897f2e508961c284683a10efabc0c2424c7
grandsmarquis <jdourlens@gmail.com>
2022-11-11 15:31:01 +0900

    api deploy

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const RolodETH = new (require("./RolodETH"))("./db_test/", 1);
+const RolodETH = new (require("../RolodETH"))("../db_test/", 1);
 
 
 app.get('/address/:address', (req, res) => {
