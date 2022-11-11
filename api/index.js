9ea70fd0d8cb713ef39709c759294334aa509b77
grandsmarquis <jdourlens@gmail.com>
2022-11-11 15:58:23 +0900

    path

--- api/index.js
+++ api/index.js
@@ -8,11 +8,13 @@ const RolodETH = new (require("../RolodETH"))(process.cwd() + "/data", 1);
 
 console.log(process.cwd())
 app.get('/address/:address', (req, res) => {
-    //res.send(process.cwd())
-    res.json(RolodETH.get(req.params.address))
+    const address = RolodETH.normalizeAddress(req.params.address);
+    let content = fs.readFileSync(process.cwd() + "/data/" + address);
+    res.setHeader('Content-Type', 'application/json');
+    res.end(content)
 })
 
-app.get('/test', function(req, res) {
+app.get('/test', function (req, res) {
     let text = fs.readFileSync(process.cwd() + "/data/0x0e9d6552b85be180d941f1ca73ae3e318d2d4f1f");
     res.send(text)
 })
