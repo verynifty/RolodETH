5f9276de2a37cb777b45290be18929f60252b149
grandsmarquis <jdourlens@gmail.com>
2022-11-11 21:29:32 +0900

    detect addresses + proxies

---
+++ scrapeBlocks.js
@@ -0,0 +1,12 @@
+const Scraper = require('./scraper')
+const RolodETH = new (require("./RolodETH"))("./data/", 1);
+
+const scraper = new Scraper("https://rpc.flashbots.net/");
+
+
+
+(async () => {
+
+    await scraper.scrapeBlocks(RolodETH, 15635081, 15635091)
+    
+})();
---
+++ scraper/index.js
@@ -0,0 +1,51 @@
+const { ethers } = require("ethers");
+const proxyDetection = require('evm-proxy-detection');
+
+function RoloScraper(rpcUrl) {
+    this.provider = new ethers.providers.JsonRpcProvider(rpcUrl);
+}
+
+RoloScraper.prototype.getAddressesInBlocks = async function (fromBlock, toBlock) {
+    let blocksPromises = [];
+    for (let index = fromBlock; index < toBlock; index++) {
+        blocksPromises.push(this.provider.getBlockWithTransactions(index));
+    }
+    const blocks = await Promise.all(blocksPromises);
+    let addresses = {}
+    for (const b of blocks) {
+        for (const t of b.transactions) {
+            addresses[t.from] = true;
+            addresses[t.to] = true;
+        }
+    }
+    return Object.keys(addresses);
+}
+
+RoloScraper.prototype.scrapeBlocks = async function (rolodeth, fromBlock, toBlock) {
+    // console.log(await  this.requestFunc("eth_getBlockByNumber",[1]))
+
+    let addresses = await this.getAddressesInBlocks(fromBlock, toBlock);
+    for (const address of addresses) {
+        const code = await this.provider.getCode(address);
+        console.log("https://etherscan.io/address/" + address, code != "0x")
+        if (code == "0x") {
+            rolodeth.addTag(address, "eoa");
+        } else {
+            rolodeth.addTag(address, "contract");
+            let provider = this.provider
+            const requestFunc = ({ method, params }) => provider.send(method, params)
+            const target = await proxyDetection.default(
+                address,
+                requestFunc,
+                "latest"
+            )
+            if (target == true) {
+                rolodeth.addTag(address, "proxy");
+            }
+            console.log(target)
+            continue;
+        }
+    }
+}
+
+module.exports = RoloScraper;
