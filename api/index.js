36e4985e7c2ab1c51fc6ff7abda48082dc9d3fe1
grandsmarquis <jdourlens@gmail.com>
2022-11-11 18:06:08 +0900

    simple api

--- api/index.js
+++ api/index.js
@@ -4,19 +4,11 @@ const port = 3000;
 
 const fs = require('fs');
 
-const RolodETH = new (require("../RolodETH"))(process.cwd() + "/data", 1);
-
-console.log(process.cwd())
 app.get('/address/:address', (req, res) => {
-    const address = RolodETH.normalizeAddress(req.params.address);
+    const address = req.params.address.toLocaleLowerCase()
     let content = fs.readFileSync(process.cwd() + "/data/" + address);
     res.setHeader('Content-Type', 'application/json');
     res.end(content)
-})
-
-app.get('/test', function (req, res) {
-    let text = fs.readFileSync(process.cwd() + "/data/0x0e9d6552b85be180d941f1ca73ae3e318d2d4f1f");
-    res.send(text)
 })
 
 app.listen(port, () => {
