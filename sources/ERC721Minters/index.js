fb5c9e4e3c27bfa68e2ef2bad2639f829e4442c1
grandsmarquis <jdourlens@gmail.com>
2022-11-13 21:12:03 +0900

    update latest

--- sources/ERC721Minters/index.js
+++ sources/ERC721Minters/index.js
@@ -3,14 +3,17 @@ const { ethers } = require("ethers");
 function ERC721Minters(rpcUrl, address, tag) {
     this.name = "ERC721Minters_" + address + "_" + Date.now();
     this.address = address;
+    this.tag = tag;
     this.provider = new ethers.providers.JsonRpcProvider(rpcUrl);
     this.contract = new ethers.Contract(address, [
         { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "from", "type": "address" }, { "indexed": true, "internalType": "address", "name": "to", "type": "address" }, { "indexed": true, "internalType": "uint256", "name": "tokenId", "type": "uint256" }], "name": "Transfer", "type": "event" }
     ], this.provider);
 }
 
-ERC721Minters.prototype.addTo = async function (RolodETH) {
+ERC721Minters.prototype.addTo = async function (RolodETH, fromBlock = 0, toBlock = "latest") {
     let filter = {
+        fromBlock: fromBlock,
+        toBlock: toBlock,
         address: this.address,
         topics: [
             ethers.utils.id("Transfer(address,address,uint256)"),
@@ -18,9 +21,15 @@ ERC721Minters.prototype.addTo = async function (RolodETH) {
             null
         ]
     };
-    console.log(filter)
     let events = await this.provider.getLogs(filter)
-    console.log(events)
+    let addresses = {}
+    for (const event of events) {
+        let address = ethers.utils.hexDataSlice(event.topics[2], 12, 32)
+        addresses[address] = true;
+    }
+    for (const address of Object.keys(addresses)) {
+        
+    }
 }
 
 module.exports = ERC721Minters;
