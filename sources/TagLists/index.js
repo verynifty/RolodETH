f9ccaa62844af0fdd1f7a412f4b46560157f9dd0
grandsmarquis <jdourlens@gmail.com>
2022-11-22 07:53:25 +0900

    sudoswap users

--- sources/TagLists/index.js
+++ sources/TagLists/index.js
@@ -5,11 +5,16 @@ function TagList(tagname) {
     this.tagname = tagname;
 }
 
-
+const sleep = ms => new Promise(r => setTimeout(r, ms));
 TagList.prototype.addTo = async function (RolodETH, fromBlock = 0, toBlock = "latest") {
     let t = require("./" + this.tagname)
+    let i = 0;
     for (const address of t) {
         RolodETH.addTag(address, this.tagname)
+        if (i % 10000 == 0) {
+            console.log(i)
+            await sleep(1000)
+        }
     }
 }
 
