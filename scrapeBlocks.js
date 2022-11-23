9ee1a7bef39e91010a8eb1dccc8e50ecb564e586
grandsmarquis <jdourlens@gmail.com>
2022-11-23 22:11:03 +0900

    using super provider

--- scrapeBlocks.js
+++ scrapeBlocks.js
@@ -1,9 +1,11 @@
 const Scraper = require('./scraper')
+const { SuperProvider }  = require("ethers-super-provider")
 const RolodETH = new (require("./RolodETH"))("./data/", 1);
 
 const scraper = new Scraper("https://rpc.flashbots.net/");
 
 (async () => {
+    console.log(SuperProvider)
     let latestBlock = await scraper.provider.getBlockNumber();
     console.log(latestBlock)
     await scraper.scrapeBlocks(RolodETH, latestBlock - 50, latestBlock)
