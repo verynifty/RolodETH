711ddceea6780db99a308a059d6810bacf9a12fe
grandsmarquis <jdourlens@gmail.com>
2022-11-29 18:27:46 +0900

    automatic scraping

--- scrapeBlocks.js
+++ scrapeBlocks.js
@@ -8,8 +8,11 @@ const rpcs = [
 const scraper = new Scraper("http://localhost:3000");
 
 (async () => {
+  while(true) {
     let latestBlock = await scraper.provider.getBlockNumber();
     console.log(latestBlock)
-    await scraper.scrapeBlocks(RolodETH, latestBlock - 20, latestBlock)
+    await scraper.scrapeBlocks(RolodETH, latestBlock - 10, latestBlock)
+  }
+
 
 })();
