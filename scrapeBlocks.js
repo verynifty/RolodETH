2fb69f7f7333f74a76591ef77f75c4078aade907
grandsmarquis <jdourlens@gmail.com>
2022-12-06 08:22:11 +0900

    automatic scraping

--- scrapeBlocks.js
+++ scrapeBlocks.js
@@ -2,14 +2,14 @@ const Scraper = require('./scraper')
 const RolodETH = new (require("./RolodETH"))("./data/", 1);
 
 const rpcs = [
-    
-  ]
+
+]
 
 const scraper = new Scraper("http://localhost:3000");
 
 (async () => {
 
-  while(true) {
+  while (true) {
     let latestBlock = await scraper.provider.getBlockNumber();
     console.log(latestBlock)
     await scraper.scrapeBlocks(RolodETH, latestBlock - 20, latestBlock)
